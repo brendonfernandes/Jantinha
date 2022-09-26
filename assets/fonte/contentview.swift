9a0b983ef560c2eec0fb890d1b264c5de2a2508b
brendonfer <brendo1122fernandess@gmail.com>
2022-09-26 02:49:02 -0300

    jantinha

---
+++ assets/fonte/contentview.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+extension Color{
+    static let background = 
+    Color(red : 08/ 255, green)
+
+}
+struct ContentView: View {
+    var body:some View {
+        ZStack{
+        FinalView()
+        }
+    }
+
+}
+struct ContentView_Previews: previews {
+    static var previews: some View {
+        ContentView()
+    }
+}   
---
+++ compras/produtos.py
@@ -0,0 +1,7 @@
+class Produto:
+    nome:str
+    preço: float
+
+    def __inti__(self, nome, preco):
+        self.nome = nome
+        self.preço = preco
